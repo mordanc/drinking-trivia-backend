fce40737b60c06ff8b951c895ba84ce58328c3e8
mordanc <iusethisacc4games@gmail.com>
2021-01-15 11:43:10 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+http.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
